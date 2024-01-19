0fc60d090f1d5a2399b1f70a9cbfcd5f00e98fd9
youngseok <dblys152@gmail.com>
2024-01-19 23:47:33 +0900

    도메인 이벤트 발행자 추가

--- infrastructure/src/main/java/com/ys/infrastructure/message/DomainEvent.java
+++ infrastructure/src/main/java/com/ys/infrastructure/message/DomainEvent.java
@@ -3,46 +3,60 @@ package com.ys.infrastructure.message;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.uuid.Generators;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
-@ToString
 public class DomainEvent<T> implements Serializable {
-
     private static final ObjectMapper objectMapper = new ObjectMapper()
             .registerModule(new JavaTimeModule())
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+    @NotBlank
     private final String id;
+
+    @NotBlank
     private final String type;
+
+    @NotNull
     private final T payload;
-    private final LocalDateTime occurredAt;
+
+    @Size(min = 1, max = 39)
+    private final String publisherId;
+
+    @NotNull
+    private final LocalDateTime publishedAt;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public DomainEvent(
             @JsonProperty("id") String id,
             @JsonProperty("type") String type,
             @JsonProperty("payload") T payload,
-            @JsonProperty("occurredAt") LocalDateTime occurredAt
+            @JsonProperty("publisherId") String publisherId,
+            @JsonProperty("publishedAt") LocalDateTime publishedAt
     ) {
         this.id = id;
         this.type = type;
         this.payload = payload;
-        this.occurredAt = occurredAt;
+        this.publisherId = publisherId;
+        this.publishedAt = publishedAt;
     }
 
-    public static <T> DomainEvent of(String type, T payload) {
+    public static <T> DomainEvent<T> create(String type, T payload, String publisherId, LocalDateTime publishedAt) {
         String id = Generators.timeBasedEpochGenerator().generate().toString();
         LocalDateTime occurredAt = LocalDateTime.now();
-        return new DomainEvent(id, type, payload, occurredAt);
+        return new DomainEvent<>(id, type, payload, publisherId, publishedAt);
     }
 
     public String serialize() {
@@ -60,17 +74,18 @@ public class DomainEvent<T> implements Serializable {
                     this.id,
                     this.type,
                     objectMapper.writeValueAsString(this.payload),
-                    this.occurredAt
+                    this.publisherId,
+                    this.publishedAt
             );
         } catch (JsonProcessingException e) {
             throw new DomainEventException(this.type, e);
         }
     }
 
-    public static DomainEvent deserialize(String serializedDomainEvent, Class<?> payloadType) {
+    public static <T> DomainEvent<T> deserialize(String serializedDomainEvent, Class<T> payloadType) {
         try {
             DomainEvent<String> domainEvent = objectMapper.readValue(serializedDomainEvent, DomainEvent.class);
-            return payloadType == String.class ? domainEvent : deserializePayload(domainEvent, payloadType);
+            return payloadType == String.class ? (DomainEvent<T>) domainEvent : deserializePayload(domainEvent, payloadType);
         } catch (JsonProcessingException e) {
             throw new DomainEventException(serializedDomainEvent, e);
         }
@@ -83,7 +98,8 @@ public class DomainEvent<T> implements Serializable {
                     serializedEvent.getId(),
                     serializedEvent.getType(),
                     deserializedPayload,
-                    serializedEvent.getOccurredAt()
+                    serializedEvent.getPublisherId(),
+                    serializedEvent.getPublishedAt()
             );
         } catch (JsonProcessingException e) {
             throw new DomainEventException(serializedEvent.getType(), e);
--- infrastructure/src/test/java/com/ys/infrastructure/message/DomainEventTest.java
+++ infrastructure/src/test/java/com/ys/infrastructure/message/DomainEventTest.java
@@ -5,24 +5,38 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DomainEventTest {
     private static final String ANY_ID = "ANY_ID";
     private static final String ANY_EVENT_TYPE = "com.ys.test.AnyEvent";
-    private static final String ANY_OCCURRED_AT_STRING = "2023-06-20T02:53:00.299046";
+    private static final String ANY_PUBLISHER_ID = "U001";
+    private static final LocalDateTime ANY_PUBLISHED_AT = LocalDateTime.of(2023, 11, 3, 13, 53, 7);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
     private DomainEvent<AnyEvent> domainEvent;
     private String serializedEvent;
 
     @BeforeEach
     void setUp() {
-        domainEvent = DomainEvent.of(ANY_EVENT_TYPE, new AnyEvent("ANY_VALUE"));
+        domainEvent = new DomainEvent<>(ANY_ID, ANY_EVENT_TYPE, new AnyEvent("ANY_VALUE"), ANY_PUBLISHER_ID, ANY_PUBLISHED_AT);
         serializedEvent = "{\"id\":\"" + ANY_ID + "\","
                 + "\"type\":\"" + ANY_EVENT_TYPE + "\","
                 + "\"payload\":\"{\\\"value\\\":\\\"ANY_VALUE\\\"}\","
-                + "\"occurredAt\":\"" + ANY_OCCURRED_AT_STRING + "\"}";
+                + "\"publisherId\":\"" + ANY_PUBLISHER_ID + "\","
+                + "\"publishedAt\":\"" + ANY_PUBLISHED_AT.format(formatter) + "\"}";
+    }
+
+    @Test
+    void 도메인_이벤트_생성() {
+        DomainEvent<AnyEvent> actual = DomainEvent.create(ANY_EVENT_TYPE, new AnyEvent("ANY_VALUE"), ANY_PUBLISHER_ID, ANY_PUBLISHED_AT);
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getId()).isNotNull();
     }
 
     @Test
@@ -34,7 +48,7 @@ class DomainEventTest {
 
     @Test
     void 도메인_이벤트_페이로드_직렬화() {
-        DomainEvent actual = domainEvent.serializePayload();
+        DomainEvent<String> actual = domainEvent.serializePayload();
 
         assertThat(actual).isNotNull();
         assertThat(actual.getPayload()).isEqualTo("{\"value\":\"ANY_VALUE\"}");
@@ -48,7 +62,8 @@ class DomainEventTest {
                 () -> assertThat(actual.getId()).isNotNull(),
                 () -> assertThat(actual.getType()).isEqualTo(ANY_EVENT_TYPE),
                 () -> assertThat(actual.getPayload()).isInstanceOf(AnyEvent.class),
-                () -> assertThat(actual.getOccurredAt()).isEqualTo(ANY_OCCURRED_AT_STRING)
+                () -> assertThat(actual.getPublisherId()).isEqualTo(ANY_PUBLISHER_ID),
+                () -> assertThat(actual.getPublishedAt()).isEqualTo(ANY_PUBLISHED_AT)
         );
     }
 
@@ -60,13 +75,14 @@ class DomainEventTest {
                 () -> assertThat(actual.getId()).isNotNull(),
                 () -> assertThat(actual.getType()).isEqualTo(ANY_EVENT_TYPE),
                 () -> assertThat(actual.getPayload()).isInstanceOf(String.class),
-                () -> assertThat(actual.getOccurredAt()).isEqualTo(ANY_OCCURRED_AT_STRING)
+                () -> assertThat(actual.getPublisherId()).isEqualTo(ANY_PUBLISHER_ID),
+                () -> assertThat(actual.getPublishedAt()).isEqualTo(ANY_PUBLISHED_AT)
         );
     }
 
     @Test
     void 도메인_이벤트_페이로드_역직렬화() {
-        DomainEvent eventOfSerializedPayload = domainEvent.serializePayload();
+        DomainEvent<String> eventOfSerializedPayload = domainEvent.serializePayload();
 
         DomainEvent<AnyEvent> actual = DomainEvent.deserializePayload(eventOfSerializedPayload, AnyEvent.class);
 
@@ -75,7 +91,8 @@ class DomainEventTest {
                 () -> assertThat(actual.getId()).isNotNull(),
                 () -> assertThat(actual.getType()).isEqualTo(ANY_EVENT_TYPE),
                 () -> assertThat(actual.getPayload()).isInstanceOf(AnyEvent.class),
-                () -> assertThat(actual.getOccurredAt()).isNotNull()
+                () -> assertThat(actual.getPublisherId()).isEqualTo(ANY_PUBLISHER_ID),
+                () -> assertThat(actual.getPublishedAt()).isEqualTo(ANY_PUBLISHED_AT)
         );
     }
 
